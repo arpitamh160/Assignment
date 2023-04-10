146d89ca8ebd9a85790df01516a542578d386eb0
Atikram <atikram.saha@codecraft.co.in>
2023-04-10 12:09:22 +0530

    Implemented solutions for problems 8,10 and 11

--- src/atikram.test.js
+++ src/atikram.test.js
@@ -1,45 +1,58 @@
+/* eslint-disable no-undef */
 /* global test,expect */
 // Importing the functions for testing purposes
 
-const{ getStringSpecial, lengthOfString, generateFirstSquares } = require("./pavan");
+const {
+  getStringSpecial,
+  lengthOfString,
+  generateFirstSquares,
+} = require("./atikram");
 
-//Testing the first function(solution 8)
+// Testing the first function(solution 8)
 
 describe("getStringSpecial", () => {
   test("returns a substring of all characters in the input string until the first repeating character", () => {
-    expect(getStringSpecial("abcdef")).toBe("abc");
+    expect(() => getStringSpecial()).toThrow();
+    expect(getStringSpecial("abcdef")).toBe("abcdef");
     expect(getStringSpecial("aabbcc")).toBe("a");
     expect(getStringSpecial("")).toBe("");
     expect(getStringSpecial("abcdeabc")).toBe("abcde");
     expect(getStringSpecial("abbcdefg")).toBe("ab");
+    expect(() => getStringSpecial(1234)).toThrow("The input is not a string");
   });
 });
 
-
-//Testing the second function(solution 10)
+// Testing the second function(solution 10)
 
 describe("lengthOfString", () => {
-    test("returns the correct length of the input string", () => {
-      expect(lengthOfString("")).toBe(0);
-      expect(lengthOfString("a")).toBe(1);
-      expect(lengthOfString("hello")).toBe(5);
-      expect(lengthOfString("12345")).toBe(5);
-      expect(lengthOfString("lengthOfString")).toBe(14);
-    });
+  test("returns the correct length of the input string", () => {
+    expect(() => lengthOfString()).toThrow();
+    expect(lengthOfString("")).toBe(0);
+    expect(lengthOfString("a")).toBe(1);
+    expect(lengthOfString("hello")).toBe(5);
+    expect(lengthOfString("12345")).toBe(5);
+    expect(lengthOfString("lengthOfString")).toBe(14);
+    expect(() => lengthOfString(753942)).toThrow("The input is not a string");
+  });
+});
+
+// Testing the third function(solution 11)
+
+describe("generateFirstSquares", () => {
+  test("returns an empty array when n is 0", () => {
+    expect(() => generateFirstSquares()).toThrow();
+    expect(generateFirstSquares(0)).toEqual([]);
   });
 
-
-  //Testing the third function(solution 11)
-
-  describe("generateFirstSquares", () => {
-    test("returns an empty array when n is 0", () => {
-      expect(generateFirstSquares(0)).toEqual([]);
-    });
-  
-    test("returns the first n perfect squares", () => {
-      expect(generateFirstSquares(1)).toEqual([1]);
-      expect(generateFirstSquares(3)).toEqual([1, 4, 9]);
-      expect(generateFirstSquares(5)).toEqual([1, 4, 9, 16, 25]);
-      expect(generateFirstSquares(10)).toEqual([1, 4, 9, 16, 25, 36, 49, 64, 81, 100]);
-    });
+  test("returns the first n perfect squares", () => {
+    expect(generateFirstSquares(1)).toStrictEqual([1]);
+    expect(generateFirstSquares(3)).toStrictEqual([1, 4, 9]);
+    expect(generateFirstSquares(5)).toStrictEqual([1, 4, 9, 16, 25]);
+    expect(generateFirstSquares(10)).toStrictEqual([
+      1, 4, 9, 16, 25, 36, 49, 64, 81, 100,
+    ]);
+    expect(() => generateFirstSquares("abcd")).toThrow(
+      "The input is not a number"
+    );
   });
+});
